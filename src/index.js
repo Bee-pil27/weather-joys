11b3ebeca8726b2669d44a6e6c60a7816dec8ca7
Bee-pil27 <Bongekah11@gmail.com>
2024-12-15 08:34:37 +0200

    Search engine properties tweaked.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ function displayTemperature(response) {
 
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
+  let searchInputElement = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInputElement.value;
 
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
